d75fb82cd1b7c41f0f229779421ff6e70a973305
August Toman-Yih <august.toman.yih@gmail.com>
2015-02-22 00:05:47 -0800

    moves arm rotation into staff

--- src/staff.js
+++ src/staff.js
@@ -2,57 +2,79 @@ function Staff(centerX, centerY) {
   var radius = 10;
   var length = 200;
 
-  var rotation = 0;
+  var staffAngle = 0;
+  var armAngle = 0;
 
-  function rotate(context, fn) {
-    function degreesToRadians(degrees) {
-      return Math.PI/180 * degrees;
-    }
+  function degreesToRadians(degrees) {
+    return Math.PI/180 * degrees;
+  }
 
+  function rotateArm(context, fn) {
+    context.save();
+
+    context.translate(350, 300);
+    context.rotate(degreesToRadians(armAngle));
+    context.translate(-350, -300);
+
+    fn();
+
+    context.restore();
+
+  }
+
+  function rotateStaff(context, fn) {
     context.save();
     context.translate(centerX, centerY);
-    context.rotate(degreesToRadians(rotation));
+    context.rotate(degreesToRadians(staffAngle));
     context.translate(-centerX, -centerY);
 
     fn();
 
     context.restore();
 
-    rotation++;
   }
 
   this.draw = function(canvas) {
     var context = canvas.context;
 
-    rotate(context, function() {
-      context.strokeStyle = 'white';
-      context.lineWidth = 3;
-      context.beginPath();
-      context.moveTo(centerX, centerY);
-      context.arc(centerX + length / 2 + radius / 2, centerY, radius, Math.PI, 3 * Math.PI, false);
-      context.arc(centerX - length / 2 - radius / 2, centerY, radius, 0, 2 * Math.PI, false);
-      context.closePath();
-      context.stroke();
+    rotateArm(context, function() {
+      rotateStaff(context, function() {
+        context.strokeStyle = 'white';
+        context.lineWidth = 3;
+        context.beginPath();
+        context.moveTo(centerX, centerY);
+        context.arc(centerX + length / 2 + radius / 2, centerY, radius, Math.PI, 3 * Math.PI, false);
+        context.arc(centerX - length / 2 - radius / 2, centerY, radius, 0, 2 * Math.PI, false);
+        context.closePath();
+        context.stroke();
+      });
     });
   };
 
   this.drawTrails = function(canvas) {
     var context = canvas.context;
 
-    rotate(context, function() {
-      context.strokeStyle = 'white';
-      context.lineWidth = 3;
+    rotateArm(context, function() {
+      rotateStaff(context, function() {
+        context.strokeStyle = 'white';
+        context.lineWidth = 3;
 
-      context.beginPath();
-      context.arc(centerX + length / 2 + radius / 2, centerY, radius, Math.PI, 3 * Math.PI, false);
-      context.closePath();
-      context.stroke();
+        context.beginPath();
+        context.arc(centerX + length / 2 + radius / 2, centerY, radius, Math.PI, 3 * Math.PI, false);
+        context.closePath();
+        context.stroke();
 
-      context.beginPath();
-      context.arc(centerX - length / 2 - radius / 2, centerY, radius, 0, 2 * Math.PI, false);
-      context.closePath();
+        context.beginPath();
+        context.arc(centerX - length / 2 - radius / 2, centerY, radius, 0, 2 * Math.PI, false);
+        context.closePath();
 
-      context.stroke();
+        context.stroke();
+      });
     });
   };
+
+  this.process = function() {
+    armAngle+=2;
+    staffAngle+=2;
+  }
 }
