77224f12c5584d0fc0cbc1e673ac0fee4a945056
August Toman-Yih <atomanyih@pivotal.io>
2015-03-04 10:50:20 -0800

    deprecate compound path in favor of muli-segment linear

--- spec/pathSpec.js
+++ spec/pathSpec.js
@@ -1,39 +1,24 @@
 describe('Linear', function() {
-  it('Interpolates between two points', function() {
-    var start = [0,0];
-    var end = [1,1];
-    var path = new Path.Linear(start, end);
-
-    expect(path.at(0)).toEqual([0,0]);
-    expect(path.at(0.5)).toEqual([0.5,0.5]);
-    expect(path.at(1)).toEqual([1,1]);
-  });
-});
-
-describe('Compound', function() {
-  describe('with one path', function() {
+  describe('with one segment', function() {
     beforeEach(function() {
-      var linear = new Path.Linear([0,0],[0,1]);
-      this.path = new Path.Compound(linear);
+      this.path = new Path.Linear([0, 0], [0, 1]);
     });
 
     it('interpolates along the path', function() {
-      expect(this.path.at(0)).toEqual([0,0]);
-      expect(this.path.at(1)).toEqual([0,1]);
+      expect(this.path.at(0)).toEqual([0, 0]);
+      expect(this.path.at(1)).toEqual([0, 1]);
     });
   });
 
-  describe('with two paths', function() {
+  describe('with two segments', function() {
     beforeEach(function() {
-      var firstPath = new Path.Linear([0,0],[0,1]);
-      var secondPath = new Path.Linear([0,1],[1,1]);
-      this.path = new Path.Compound(firstPath, secondPath);
+      this.path = new Path.Linear([0, 0], [0, 1], [1, 1]);
     });
 
     it('interpolates along joined paths', function() {
-      expect(this.path.at(0)).toEqual([0,0]);
-      expect(this.path.at(0.5)).toEqual([0,1]);
-      expect(this.path.at(1)).toEqual([1,1]);
+      expect(this.path.at(0)).toEqual([0, 0]);
+      expect(this.path.at(0.5)).toEqual([0, 1]);
+      expect(this.path.at(1)).toEqual([1, 1]);
     });
   });
 });
--- src/path.js
+++ src/path.js
@@ -1,26 +1,24 @@
 var Path = {};
 
-Path.Linear = function(start, end) {
-  this.at = function(i) {
+Path.Linear = function() {
+  var points = arguments;
+  var numSegments = points.length - 1;
+
+  function interpolate(i, start, end) {
     var x = (end[0] - start[0]) * i + start[0];
     var y = (end[1] - start[1]) * i + start[1];
 
-    return [x,y];
-  };
-};
-
-Path.Compound = function() {
-  var paths = arguments;
+    return [x, y];
+  }
 
   this.at = function(i) {
-    var pathIndex = Math.floor(i * paths.length);
-    var pathI = i * paths.length - pathIndex;
-    var path = paths[pathIndex];
-
-    if(path) {
-      return path.at(pathI);
+    if (i == 1) {
+      return points[points.length - 1];
     } else {
-      return paths[paths.length - 1].at(1);
+      var segmentIndex = Math.floor(i * numSegments);
+      var segmentI = i * numSegments - segmentIndex;
+
+      return interpolate(segmentI, points[segmentIndex], points[segmentIndex + 1]);
     }
   };
 };
